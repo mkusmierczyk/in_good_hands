83e3e65cfde05e109306a703bf2ba661288f1f80
Mateusz <makusmierczyk@gmail.com>
2020-05-19 14:21:41 +0200

    partial

--- development/js/components/home/headerStartHelp.js
+++ development/js/components/home/headerStartHelp.js
@@ -6,12 +6,15 @@ import {BrowserRouter, HashRouter, Link, Route} from "react-router-dom";
 export const StartHelp = () => {
 
     return (<>
-        <div className="row">
+        <div className="col-12">
             <div className="col-12">
-                <button> <Link to={"/logIn"}>Zaloguj </Link></button>
-                <button> <Link to={"/register"}> Załóż konto </Link> </button>
+                <h1 > Zacznij pomogać !<span > Oddaj niechciane rzeczy w zaufane ręce </span></h1>
+                <span > obrazek</span>
             </div>
-
+            <div className="row">
+                <button className= "col-6"> <Link to={"/LogIn"}>  ODDAJ RZECZY </Link></button>
+            <button className="col-6"> <Link to={"/LogIn"}> ZORGANIZUJ ZBIÓRKĘ </Link> </button>
+            </div>
         </div>
     </>)
 }
