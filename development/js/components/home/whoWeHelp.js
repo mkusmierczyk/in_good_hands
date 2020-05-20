0bda4e1905237ef24d476e467cf13a5277da4273
Mateusz <makusmierczyk@gmail.com>
2020-05-20 20:53:14 +0200

    partial commit

--- development/js/components/home/whoWeHelp.js
+++ development/js/components/home/whoWeHelp.js
@@ -4,30 +4,67 @@ import {ReactDOM, NavLink} from "react-dom";
 
 export const WhoWeHelp = () => {
 
-    const [orgTitle, setOrgTitle] = useState("W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.")
+    const[toogleClass, setToggleClass]=useState({fund:"active",ngo:"",local:""})
+
+
+    const [pagination, setPagination] = useState({
+        pagination: {
+            title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+            organization: {
+                name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
+                mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+            }
+        }
+    })
 
 
     const HandleClick = (e) => {
         e.preventDefault()
         if (e.target.innerText === "Fundacjom") {
-            e.target.classList.add("active")
-            e.target.nextElementSibling.classList.remove("active")
-            e.target.nextElementSibling.nextElementSibling.classList.remove("active")
-            setOrgTitle("W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.")
+           setToggleClass({fund: "active", ngo: "", local: ""})
+
+            setPagination({
+                pagination: {
+                    title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    organization: {
+                        name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
+                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                        things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+                    }
+                }
+            })
 
 
         } else if (e.target.innerText === "Organizacjom Pozarządowym") {
-            e.target.classList.add("active")
-            e.target.nextSibling.classList.remove("active")
-            e.target.previousElementSibling.classList.remove("active")
-            setOrgTitle("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet, consectetur culpa cum dignissimos distinctio dolor dolore ea eaque enim, esse impedit in libero modi molestiae odit quasi temporibus vel.")
+            setToggleClass({fund: "", ngo: "active", local: ""})
+            setPagination({
+                pagination: {
+                    title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    organization: {
+                        name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
+                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                        things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+                    }
+                }
+            })
 
 
-        } else e.target.classList.add("active")
-        e.target.previousElementSibling.classList.remove("active")
-        e.target.previousElementSibling.previousElementSibling.classList.remove("active")
-        setOrgTitle(" dolore ea eaque enim, esse impedit in libero modi molestiae odit quasi temporibus vel.")
+        } else
+        setToggleClass({fund: "", ngo: "", local: "active"})
+        setPagination({
+            pagination: {
+                title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                organization: {
+                    name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
+                    mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                    things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+                }
+            }
+        })
     }
+
+
 
 
     return (<>
@@ -37,22 +74,21 @@ export const WhoWeHelp = () => {
                         <h1> Komu pomagamy ? </h1>
                         <span className="whoWeHelp__header__image decorationImage"> </span>
                     </div>
-                    <div className="row whoWeHelp__content_organisations">
-                        <div className=" col-2 whoWeHelp__content_organisations__fundations active"
+                    <div className="row whoWeHelp__content_organisations" >
+                        <div className={`col-2 whoWeHelp__content_organisations__fundations  ${toogleClass.fund}`}
                              onClick={HandleClick}>Fundacjom
                         </div>
-                        <div className="col-2 whoWeHelp__content_organisations_ngo" onClick={HandleClick}>Organizacjom
+                        <div className={`col-2 whoWeHelp__content_organisations_ngo ${toogleClass.ngo}`} onClick={HandleClick}>Organizacjom
                             Pozarządowym
                         </div>
-                        <div className=" col-2 whoWeHelp__content_organisations__local" onClick={HandleClick}>Lokalnym
+                        <div className={ `col-2 whoWeHelp__content_organisations__local ${toogleClass.local}`} onClick={HandleClick}>Lokalnym
                             zbiórkom
                         </div>
                     </div>
 
                     <div className="row whoWeHelp__content__pagination">
-                        <h1 className="col-5 whoWeHelp__content__pagination__title">{orgTitle}</h1>
+                        <h1 className="col-5 whoWeHelp__content__pagination__title">{pagination.title}</h1>
                     </div>
-
 
 
                 </div>
