5436ca93ee809e68f8d899db5a66eba8a23950c9
Mateusz <makusmierczyk@gmail.com>
2020-05-21 00:25:04 +0200

    pagination partial

--- development/js/components/home/whoWeHelp.js
+++ development/js/components/home/whoWeHelp.js
@@ -4,15 +4,16 @@ import {ReactDOM, NavLink} from "react-dom";
 
 export const WhoWeHelp = () => {
 
-    const[toogleClass, setToggleClass]=useState({fund:"active",ngo:"",local:""})
-
 
     const [pagination, setPagination] = useState({
         pagination: {
             title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+            fund: "active",
+            ngo: "",
+            local: "",
             organization: {
                 name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.", "Cel i misja: Pomoc dzieciom z ubogich rodzin.", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania."],
                 things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
             }
         }
@@ -22,14 +23,17 @@ export const WhoWeHelp = () => {
     const HandleClick = (e) => {
         e.preventDefault()
         if (e.target.innerText === "Fundacjom") {
-           setToggleClass({fund: "active", ngo: "", local: ""})
+
 
             setPagination({
                 pagination: {
                     title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    fund: "active",
+                    ngo: "",
+                    local: "",
                     organization: {
                         name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
+                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.", "Cel i misja: Pomoc dzieciom z ubogich rodzin.", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania."],
                         things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
                     }
                 }
@@ -37,34 +41,38 @@ export const WhoWeHelp = () => {
 
 
         } else if (e.target.innerText === "Organizacjom Pozarządowym") {
-            setToggleClass({fund: "", ngo: "active", local: ""})
+
             setPagination({
                 pagination: {
-                    title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    title: "W naszej bazie znajdziesz listę zweryfikowanych NGO, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    fund: "",
+                    ngo: "active",
+                    local: "",
                     organization: {
-                        name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
-                        things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+                        name: ["Fundacja “ Zdrowie”", "Fundacja “Dzieci”", "Fundacja “W domu”"],
+                        mission: ["Cel i misja: Pomoc osobom znajdującym się w .", "Cel i misja: Pomoc dzieciom .", "Cel i misja: Pomoc dla osób nie posiadających ."],
+                        things: ["ubrania,  zabawki", "ubrania, jedzenie,  meble, zabawki", "ubrania, jedzenie"]
                     }
                 }
             })
 
 
         } else
-        setToggleClass({fund: "", ngo: "", local: "active"})
-        setPagination({
-            pagination: {
-                title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-                organization: {
-                    name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                    mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.",],
-                    things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
+
+            setPagination({
+                pagination: {
+                    title: "W naszej bazie znajdziesz listę zweryfikowanych osób zbierających, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                    fund: "",
+                    ngo: "",
+                    local: "active",
+                    organization: {
+                        name: ["Fundacja “Dbam ", "Fundacja “Dbam bardzo”", "Fundacja bardzo bardzo"],
+                        mission: ["Cel i misja:  w trudnej sytuacji życiowej.", "Cel i misja: ubogich rodzin.", "Cel i misja: posiadających miejsca zamieszkania."],
+                        things: [" meble, zabawki", "ubrania, jedzenie, sprzęt AGD, zabawki", " jedzenie, "]
+                    }
                 }
-            }
-        })
+            })
     }
-
-
 
 
     return (<>
@@ -74,21 +82,31 @@ export const WhoWeHelp = () => {
                         <h1> Komu pomagamy ? </h1>
                         <span className="whoWeHelp__header__image decorationImage"> </span>
                     </div>
-                    <div className="row whoWeHelp__content_organisations" >
-                        <div className={`col-2 whoWeHelp__content_organisations__fundations  ${toogleClass.fund}`}
-                             onClick={HandleClick}>Fundacjom
+                    <div className="row whoWeHelp__content_organisations">
+                        <div
+                            className={`col-2 whoWeHelp__content_organisations__fundations  ${pagination.pagination.fund}`}
+                            onClick={HandleClick}>Fundacjom
                         </div>
-                        <div className={`col-2 whoWeHelp__content_organisations_ngo ${toogleClass.ngo}`} onClick={HandleClick}>Organizacjom
-                            Pozarządowym
+                        <div className={`col-2 whoWeHelp__content_organisations_ngo ${pagination.pagination.ngo}`}
+                             onClick={HandleClick}>Organizacjom Pozarządowym
                         </div>
-                        <div className={ `col-2 whoWeHelp__content_organisations__local ${toogleClass.local}`} onClick={HandleClick}>Lokalnym
+                        <div className={`col-2 whoWeHelp__content_organisations__local ${pagination.pagination.local}`}
+                             onClick={HandleClick}>Lokalnym
                             zbiórkom
                         </div>
                     </div>
 
                     <div className="row whoWeHelp__content__pagination">
-                        <h1 className="col-5 whoWeHelp__content__pagination__title">{pagination.title}</h1>
+                        <h1 className="col-5 whoWeHelp__content__pagination__title">{pagination.pagination.title}</h1>
                     </div>
+                    <ul>
+                        {pagination.pagination.organization.name.map((name, index) => <li key={index}> {name}</li>)}
+                        {pagination.pagination.organization.mission.map((mission, index) => <li key={index}
+                        > {mission}</li>)}
+                        {pagination.pagination.organization.things.map((things, index) => <li key={index}
+                        > {things} </li>)}
+
+                    </ul>
 
 
                 </div>
