a22a886e6f687750906a198d8f5599b302789aeb
Mateusz <makusmierczyk@gmail.com>
2020-05-21 16:16:53 +0200

    partial

--- development/js/components/home/whoWeHelp.js
+++ development/js/components/home/whoWeHelp.js
@@ -5,19 +5,94 @@ import {ReactDOM, NavLink} from "react-dom";
 export const WhoWeHelp = () => {
 
 
-    const [pagination, setPagination] = useState({
-        pagination: {
-            title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-            fund: "active",
-            ngo: "",
-            local: "",
-            organization: {
-                name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.", "Cel i misja: Pomoc dzieciom z ubogich rodzin.", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania."],
-                things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
-            }
-        }
-    })
+    //Start Zmian
+    const alternateSection = {
+        whoWeHelp: [
+            {
+                title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                fund: "active",
+                ngo: "",
+                local: "",
+                organizations: [
+
+                    {
+                        name: "Fundacja “Dbam ",
+                        mission: "Cel i misja:  w trudnej sytuacji życiowej.",
+                        things: " meble, zabawki",
+                    },
+
+                    {
+                        name: "Fundacja “Dbam bardzo”",
+                        mission: "Cel i misja: ubogich rodzin.",
+                        things: "ubrania, jedzenie, sprzęt AGD, zabawki",
+                    },
+
+                    {
+                        name: "Fundacja bardzo bardzo",
+                        mission: "Cel i misja: posiadających miejsca zamieszkania.",
+                        things: " jedzenie, "
+                    },
+                ]
+            },
+            {
+                title: "W naszej bazie znajdziesz listę zweryfikowanych NGO, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                fund: "",
+                ngo: "active",
+                local: "",
+                organizations: [
+
+                    {
+                        name: "Fundacja “Zdrowie ",
+                        mission: "Cel i misja: Zdrowie.",
+                        things: [" meble, zabawki", "ubrania, jedzenie, sprzęt AGD, zabawki", " jedzenie, "]
+                    },
+
+                    {
+                        name: "Fundacja “Dzieci”",
+                        mission: "Cel i misja: Lorem Ipsum Dolor.",
+                        things: " meble, zabawki",
+                    },
+
+                    {
+                        name: "Fundacja Bezradni",
+                        mission: "Cel i misja:  Pomoc w trudnej sytuacji.",
+                        things: "ubrania, jedzenie, sprzęt AGD, zabawki",
+                    },
+                ]
+            },
+            {
+                title: "W naszej bazie znajdziesz listę zweryfikowanych osób zbierających, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+                fund: "",
+                ngo: "",
+                local: "active",
+                organizations: [
+
+                    {
+                        name: "Fundacja “Dbam bardzo”",
+                        mission: "Cel i misja:  Pomoc",
+                        things: " meble, zabawki",
+                    },
+
+                    {
+                        name: ["Fundacja “Dbam ", "Fundacja “Dbam bardzo”", "Fundacja bardzo bardzo"],
+                        mission: ["Cel i misja:  w trudnej sytuacji życiowej.", "Cel i misja: ubogich rodzin.", "Cel i misja: posiadających miejsca zamieszkania."],
+                        things: [" meble, zabawki", "ubrania, jedzenie, sprzęt AGD, zabawki", " jedzenie, "]
+                    },
+
+                    {
+                        name: "Fundacja “Zdrowie ",
+                        mission: "Cel i misja: Zdrowie.",
+                        things: [" meble, zabawki", "ubrania, jedzenie, sprzęt AGD, zabawki", " jedzenie, "]
+                    },
+                ]
+
+            },
+
+        ]
+    }
+
+
+    const [organizationSection, setOrganizationSection] = useState(alternateSection.whoWeHelp[0])
 
 
     const HandleClick = (e) => {
@@ -25,54 +100,21 @@ export const WhoWeHelp = () => {
         if (e.target.innerText === "Fundacjom") {
 
 
-            setPagination({
-                pagination: {
-                    title: "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-                    fund: "active",
-                    ngo: "",
-                    local: "",
-                    organization: {
-                        name: ["Fundacja “Dbam o Zdrowie”", "Fundacja “Dbam dla Dzieci”", "Fundacja “Bez domu”"],
-                        mission: ["Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.", "Cel i misja: Pomoc dzieciom z ubogich rodzin.", "Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania."],
-                        things: ["ubrania, meble, zabawki", "ubrania, jedzenie, sprzęt AGD, meble, zabawki", "ubrania, jedzenie, ciepłe koce"]
-                    }
-                }
-            })
-
+            setOrganizationSection(alternateSection.whoWeHelp[0])
 
         } else if (e.target.innerText === "Organizacjom Pozarządowym") {
 
-            setPagination({
-                pagination: {
-                    title: "W naszej bazie znajdziesz listę zweryfikowanych NGO, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-                    fund: "",
-                    ngo: "active",
-                    local: "",
-                    organization: {
-                        name: ["Fundacja “ Zdrowie”", "Fundacja “Dzieci”", "Fundacja “W domu”"],
-                        mission: ["Cel i misja: Pomoc osobom znajdującym się w .", "Cel i misja: Pomoc dzieciom .", "Cel i misja: Pomoc dla osób nie posiadających ."],
-                        things: ["ubrania,  zabawki", "ubrania, jedzenie,  meble, zabawki", "ubrania, jedzenie"]
-                    }
-                }
-            })
-
+            setOrganizationSection(alternateSection.whoWeHelp[1])
 
         } else
 
-            setPagination({
-                pagination: {
-                    title: "W naszej bazie znajdziesz listę zweryfikowanych osób zbierających, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
-                    fund: "",
-                    ngo: "",
-                    local: "active",
-                    organization: {
-                        name: ["Fundacja “Dbam ", "Fundacja “Dbam bardzo”", "Fundacja bardzo bardzo"],
-                        mission: ["Cel i misja:  w trudnej sytuacji życiowej.", "Cel i misja: ubogich rodzin.", "Cel i misja: posiadających miejsca zamieszkania."],
-                        things: [" meble, zabawki", "ubrania, jedzenie, sprzęt AGD, zabawki", " jedzenie, "]
-                    }
-                }
-            })
+            setOrganizationSection(alternateSection.whoWeHelp[2])
     }
+
+
+    let {title, fund, ngo, local} = organizationSection
+
+    let allBeneficiary = organizationSection.organizations
 
 
     return (<>
@@ -84,36 +126,35 @@ export const WhoWeHelp = () => {
                     </div>
                     <div className="row whoWeHelp__content_organisations">
                         <div
-                            className={`col-2 whoWeHelp__content_organisations__fundations  ${pagination.pagination.fund}`}
+                            className={`col-2 whoWeHelp__content_organisations__fundations  ${fund}`}
                             onClick={HandleClick}>Fundacjom
                         </div>
-                        <div className={`col-2 whoWeHelp__content_organisations_ngo ${pagination.pagination.ngo}`}
+                        <div className={`col-2 whoWeHelp__content_organisations_ngo ${ngo}`}
                              onClick={HandleClick}>Organizacjom Pozarządowym
                         </div>
-                        <div className={`col-2 whoWeHelp__content_organisations__local ${pagination.pagination.local}`}
+                        <div className={`col-2 whoWeHelp__content_organisations__local ${local}`}
                              onClick={HandleClick}>Lokalnym
                             zbiórkom
                         </div>
                     </div>
 
-                    <div className="row whoWeHelp__content__pagination">
-                        <h1 className="col-5 whoWeHelp__content__pagination__title">{pagination.pagination.title}</h1>
+
+                    <div className="row whoWeHelp__content__beneficiary">
+                        <h1 className="col-5 whoWeHelp__content__beneficiary__title">{title}</h1>
                     </div>
-                    <ul>
-                        {pagination.pagination.organization.name.map((name, index) => <li key={index}> {name}</li>)}
-                        {pagination.pagination.organization.mission.map((mission, index) => <li key={index}
-                        > {mission}</li>)}
-                        {pagination.pagination.organization.things.map((things, index) => <li key={index}
-                        > {things} </li>)}
+                    <ul className="whoWeHelp__content__beneficiary__list">
+                        {allBeneficiary.map((elem, index) => <li
+                            className="col-9 whoWeHelp__content__beneficiary__list__elem " key={index}>
+                            <h2 className="col-6 whoWeHelp__content__beneficiary__elem__name">{elem.name}<span
+                                className="col-6 whoWeHelp__content__beneficiary__elem__things">{elem.things}</span>
+                            </h2>
 
+                            <p className="col-6 whoWeHelp__content__beneficiary__elem__mission">{elem.mission}</p>
+                        </li>)}
                     </ul>
-
-
                 </div>
 
             </div>
-
-
         </>
     )
 }
